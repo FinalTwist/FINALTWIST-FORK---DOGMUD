3ea5368cd1d3df6da04408008c689e3db2967a34
FinalTwist <65344940+FinalTwist@users.noreply.github.com>
2026-09-24 11:42:37 -0500

    resetting again (wrong files added)

--- internal/mutations/flight.go
+++
@@ -1,8 +0,0 @@
-package mutations
-
-// IsFlying reports whether any owned mutation grants the "flying" flag (the
-// Winged Flight transformation). Read by the combat, flee, and movement hooks
-// that give a flyer its edge over the earthbound.
-func IsFlying(owned map[string]int) bool {
-	return HasMutationFlag(owned, "flying")
-}
--- internal/rooms/sign.go
+++
@@ -1,9 +0,0 @@
-package rooms
-
-import "time"
-
-type Sign struct {
-	VisibleUserId int       // What user can see it? If 0, then everyone can see it.
-	DisplayText   string    // What text to display
-	Expires       time.Time // When this sign expires.
-}
